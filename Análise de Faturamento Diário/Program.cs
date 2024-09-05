327fd70d9fa61e33a203b659b1804f31b681f42d
Carlos Felipe da Paixão <paixao.carlosfelipe@gmail.com>
2024-09-05 12:02:11 -0300

    Adicionar arquivos de projeto.

---
+++ Análise de Faturamento Diário/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Vetor de faturamento diário de um mês
+        double[] faturamento = { 2000, 2400, 1500, 1800, 2100, 0, 0, 2700, 2200, 1900, 2500, 2800, 0, 0, 3000, 3200, 3100, 2000, 2200, 0, 0, 3500, 3300, 3700, 4000, 4200, 0, 0, 1800, 1900 };
+
+        double menor = faturamento[0], maior = faturamento[0];
+        double totalFaturamento = 0;
+        int diasComFaturamento = 0, diasAcimaMedia = 0;
+
+        // Calcula menor, maior e soma dos faturamentos
+        for (int i = 0; i < faturamento.Length; i++)
+        {
+            if (faturamento[i] > 0)
+            {
+                if (faturamento[i] < menor) menor = faturamento[i];
+                if (faturamento[i] > maior) maior = faturamento[i];
+                totalFaturamento += faturamento[i];
+                diasComFaturamento++;
+            }
+        }
+
+        // Calcula a média mensal
+        double mediaMensal = totalFaturamento / diasComFaturamento;
+
+        // Conta dias com faturamento acima da média
+        foreach (var valor in faturamento)
+        {
+            if (valor > mediaMensal) diasAcimaMedia++;
+        }
+
+        // Exibe os resultados
+        Console.WriteLine($"Menor faturamento: R$ {menor:N2}");
+        Console.WriteLine($"Maior faturamento: R$ {maior:N2}");
+        Console.WriteLine($"Dias com faturamento acima da média: {diasAcimaMedia}");
+
+        Console.WriteLine("Pressione qualquer tecla para encerrar...");
+        Console.ReadKey();
+    }
+}
---
+++ Análise de Faturamento Diário/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Análise de Faturamento Diário")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Análise de Faturamento Diário")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("dd7e7960-8769-4fb0-a4b3-8e1cb8412bb8")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
